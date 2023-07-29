248dd6055c696b998cdadb03d5f17c4ac687336e
Berdishukur <boboqulovberdishukur@gmail.com>
2023-07-29 20:50:26 +0500

    LMS

---
+++ amaliyot.py
@@ -0,0 +1,19 @@
+# #Bu dastur Python tilida yozildi
+#
+# def is_true(k): # Bu funksiya har bitda sonni tublikga tekshirib beradi
+#     sum=0
+#     for i in range(1,k+1):
+#         if k%i==0:
+#             sum+=1
+#     if sum==2:
+#         return True
+#     return False
+#
+# def tub_list(n):  #Bu funksiya esa berilgan songacha bo'lgan tub sonlarni ro'yxatini qaytaradi
+#     list=[]
+#     for i in range(1,n+1):
+#         if is_true(i):
+#             list.append(i)
+#     print(list)
+# tub_list(20)
+
